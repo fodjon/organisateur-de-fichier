b3265e1efc13fefe63856e80d8a7793ff9d00c48
fodjon <fodjonguetsisi@gmail.com>
2026-05-18 20:45:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = input("Chemin du ficher : ")
+extention = input("Choisissez l'extension à trier : ")
+if os.path.exists(path):
+    fichiers = os.listdir(path)
+    for fichier in fichiers:
+        if fichier.endswith(f".{extention}"):
+            sous_dossier = f"{path}{extention}"
+            chemin_actuel = os.path.join(path, fichier)
+            chemin_sous_dossier = os.path.join(sous_dossier, fichier)
+            os.makedirs(sous_dossier, exist_ok=True)
+            shutil.move(chemin_actuel, chemin_sous_dossier)
+
+else:
+    print("Le ficher n'existe pas")
