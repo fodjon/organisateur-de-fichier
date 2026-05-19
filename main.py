09f6ce80302bbcff90c31cb3e0ab745317f451c2
fodjon <fodjonguetsisi@gmail.com>
2026-05-19 09:25:43 +0200

    final commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ if os.path.exists(path):
     fichiers = os.listdir(path)
     for fichier in fichiers:
         if fichier.endswith(f".{extention}"):
-            sous_dossier = f"{path}{extention}"
+            sous_dossier = os.path.join(path, extention)
             chemin_actuel = os.path.join(path, fichier)
             chemin_sous_dossier = os.path.join(sous_dossier, fichier)
             os.makedirs(sous_dossier, exist_ok=True)
             shutil.move(chemin_actuel, chemin_sous_dossier)
-
+            print(f"{fichier} -> {chemin_sous_dossier}")
 else:
     print("Le ficher n'existe pas")
